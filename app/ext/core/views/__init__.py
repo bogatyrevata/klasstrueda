e64b7593fd4eff6a5330c58fc3851010be70c76d
Anton Popov <ademaro@ya.ru>
2024-02-23 20:06:01 +0530

    Подправлены переменные для шаблонов + init

--- app/ext/core/views/__init__.py
+++ app/ext/core/views/__init__.py
@@ -14,30 +14,29 @@ core = Blueprint("core", __name__, template_folder="templates")
 
 
 def init_request():
-    # try:
-    #     db.create_all()
-    #     user_datastore.find_or_create_role(
-    #         name="admin",
-    #         permissions={"admin-read", "admin-write", "user-read", "user-write"},
-    #     )
-    #     user_datastore.find_or_create_role(name="user", permissions={"user-read", "user-write"})
-    #     if not user_datastore.find_user(email="admin@z-gu.ru"):
-    #         user_datastore.create_user(
-    #             username="admin",
-    #             email="admin@z-gu.ru",
-    #             password=hash_password("1234567"),
-    #             roles=["admin"],
-    #         )
-    #     if not user_datastore.find_user(email="user@z-gu.ru"):
-    #         user_datastore.create_user(
-    #             username="user",
-    #             email="user@z-gu.ru",
-    #             password=hash_password("1234567"),
-    #             roles=["user"],
-    #         )
-    # except OperationalError as msg:
-    #     abort(500, msg)
-    pass
+    """Создание базовых ролей и пользователей."""
+    try:
+        user_datastore.find_or_create_role(
+            name="admin",
+            permissions={"admin-read", "admin-write", "user-read", "user-write"},
+        )
+        user_datastore.find_or_create_role(name="user", permissions={"user-read", "user-write"})
+        if not user_datastore.find_user(email="admin@z-gu.ru"):
+            user_datastore.create_user(
+                username="admin",
+                email="admin@z-gu.ru",
+                password=hash_password("1234567"),
+                roles=["admin"],
+            )
+        if not user_datastore.find_user(email="user@z-gu.ru"):
+            user_datastore.create_user(
+                username="user",
+                email="user@z-gu.ru",
+                password=hash_password("1234567"),
+                roles=["user"],
+            )
+    except OperationalError as msg:
+        abort(500, msg)
 
 
 @core.before_app_request
@@ -83,7 +82,8 @@ def before_app_request():
       }
     ]
     g.contacts = {
-      "phone": "+79112454000",
+      "phone": "+7(911)245-40-00",
+      "tel": "+79112454000",
       "address": "Санкт-Петербург, Газовая 10",
       "email": "klasstrueda@gmail.com"
     }
