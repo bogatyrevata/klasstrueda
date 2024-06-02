466c7d32c152fec5e1884ab76c7717bf6b1a0b2e
Anton Popov <ademaro@ya.ru>
2024-06-02 23:43:53 +0530

    Подправлены точки монтирования для блюпринтов курсов

--- app/ext/courses/__init__.py
+++ app/ext/courses/__init__.py
@@ -4,7 +4,7 @@ __version__ = "0.1.0"
 __author__ = "KLASSTRUEDA"
 
 from .views import courses
-from .views.product import product
-from .views.customer import customer
+from .views.user import user_courses
+from .views.admin import admin_courses
 
-bps = [(courses, "/"), (customer, "/user"), (product, "/course")]
+bps = [(courses, "/courses"), (user_courses, "/lk/courses"), (admin_courses, "/admin/courses")]
