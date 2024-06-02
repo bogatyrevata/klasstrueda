40dcf0f12bb7ff0a9ec2c10998795621a561af50
Anton Popov <ademaro@ya.ru>
2024-06-02 23:43:00 +0530

    Добавлена форма для категорий курсов

--- app/ext/courses/forms.py
+++ app/ext/courses/forms.py
@@ -3,3 +3,10 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed
 from wtforms import StringField, SubmitField, TelField, FileField, EmailField, TextAreaField
 from wtforms.validators import DataRequired, Email
+
+
+class CategoryForm(FlaskForm):
+    name = StringField("Название категории", validators=[DataRequired()])
+    alias = StringField("Алиас категории", validators=[DataRequired()])  # TODO: добавить валидатор на латиницу
+    description = TextAreaField("Описание категории")
+    submit = SubmitField("Добавить категорию")
