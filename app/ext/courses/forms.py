6eff2d31fe620dd3a4f19bd083baf59955e4236a
Taisiya Bogatyreva <bogatyrevata@gmail.com>
2024-06-05 13:04:48 -0300

    Добавлена форма для курса и исправлен alias

--- app/ext/courses/forms.py
+++ app/ext/courses/forms.py
@@ -1,12 +1,25 @@
 from flask_security import ConfirmRegisterForm
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed
-from wtforms import StringField, SubmitField, TelField, FileField, EmailField, TextAreaField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, SubmitField, TelField, FileField, EmailField, TextAreaField, SelectField
+from wtforms.validators import DataRequired, Regexp
 
 
 class CategoryForm(FlaskForm):
     name = StringField("Название категории", validators=[DataRequired()])
-    alias = StringField("Алиас категории", validators=[DataRequired()])  # TODO: добавить валидатор на латиницу
+    alias = StringField("Алиас категории", validators=[
+        DataRequired(),
+        Regexp(r'^[a-zA-Z0-9_-]+$', message="Алиас должен содержать только латинские буквы, цифры, дефисы и подчеркивания.")
+    ])
     description = TextAreaField("Описание категории")
     submit = SubmitField("Добавить категорию")
+
+class CourseForm(FlaskForm):
+    name = StringField("Название курса", validators=[DataRequired()])
+    alias = StringField("Алиас курса", validators=[
+        DataRequired(),
+        Regexp(r'^[a-zA-Z0-9_-]+$', message="Алиас должен содержать только латинские буквы, цифры, дефисы и подчеркивания.")
+    ])
+    category_id = SelectField("Название категории", coerce=int, validators=[DataRequired()])
+    description = TextAreaField("Описание курса")
+    submit = SubmitField("Добавить курс")
