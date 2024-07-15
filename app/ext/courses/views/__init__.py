a8ee7ad5faf5cef8c52fcfeee92220190da2b8de
Taisiya Bogatyreva <bogatyrevata@gmail.com>
2024-07-15 02:50:08 -0300

    Вью для отображение одного курса, для генерации ссылки на него по id

--- app/ext/courses/views/__init__.py
+++ app/ext/courses/views/__init__.py
@@ -14,3 +14,10 @@ def index():
     categories_db = Category.query.all()
     courses_db = Course.query.all()
     return render_template("courses/public/index.j2", categories=categories_db, courses=courses_db)
+
+@courses.route("/course/<int:course_id>")
+def course_details(course_id):
+    course = Course.query.get(course_id)
+    if not course:
+        abort(404)  # Если курс не найден, возвращаем ошибку 404
+    return render_template("courses/public/course_details.j2", course=course)
