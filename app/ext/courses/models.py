8d3e5dcdc547130c7af2dc089669cc08eae808fc
Taisiya Bogatyreva <bogatyrevata@gmail.com>
2024-11-01 01:54:02 -0300

    Импорт timezone

--- app/ext/courses/models.py
+++ app/ext/courses/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 from app.extensions import db
 from app.models import ModelMixin
@@ -248,8 +248,9 @@ class Course(db.Model, ModelMixin):
     price = db.Column(db.Text)
     student_works = db.relationship("StudentWork", secondary="course_studentwork", backref="course")
     final_registration_form = db.Column(db.String(255))
-    start_date = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
-    end_date = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+
+    start_date = db.Column(db.DateTime, default=lambda: datetime.now(timezone.UTC))
+    end_date = db.Column(db.DateTime, default=lambda: datetime.now(timezone.UTC))
 
     modules = db.relationship("Module", secondary="course_module", backref="course")
 
