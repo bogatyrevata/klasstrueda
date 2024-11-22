cc388077cf06d14e8d11668bf6e2e23456b44527
Taisiya Bogatyreva <bogatyrevata@gmail.com>
2024-11-22 17:56:52 +0300

    Подправлен конфиг и exceptions для хранения видео и файлов

--- app/extensions.py
+++ app/extensions.py
@@ -5,7 +5,7 @@ from flask_resize import Resize
 from flask_security import Security
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
-from flask_uploads import IMAGES, UploadSet
+from flask_uploads import IMAGES,AUDIO, DOCUMENTS, UploadSet
 from flask_wtf.csrf import CSRFProtect
 
 
@@ -16,5 +16,7 @@ mail = Mail()
 migrate = Migrate()
 resize = Resize()
 photos = UploadSet("photos", IMAGES)
+videos = UploadSet("videos", AUDIO)
+files = UploadSet("files", DOCUMENTS)
 security = Security()
 session = Session()
