f6b694e450d1a298f490ae178fdb40575d945874
Taisiya Bogatyreva <bogatyrevata@gmail.com>
2024-12-11 23:44:07 +0300

    Разметка, стили и js для загрузки аватара пользователя в личном кабинете

--- app/static/mjs/script.js
+++ app/static/mjs/script.js
@@ -224,3 +224,24 @@ $(".workphoto-carousel").owlCarousel({
 
     // Заполняем поле временной метки при загрузке формы, для того чтобы сделать проверку на бота
     document.getElementById('form_time').value = Date.now();
+
+
+//Загрузка аватара в личном кабинете пользователя
+
+const fileInput = document.getElementById("file-input");
+const imagePreview = document.getElementById("img-preview");
+const toast = document.getElementById("toast");
+
+fileInput.addEventListener("change", (e) => {
+  if (e.target.files.length) {
+    const src = URL.createObjectURL(e.target.files[0]);
+    imagePreview.src = src;
+    showToast();
+  }
+});
+
+function showToast() {
+  toast.classList.add("show");
+  setTimeout(() => toast.classList.remove("show"), 3000);
+}
+
