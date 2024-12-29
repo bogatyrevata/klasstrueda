3861da901fb6664a29be3bf621c31aa350557328
Taisiya Bogatyreva <bogatyrevata@gmail.com>
2024-12-29 13:00:57 -0300

    Изменена вью для обработки формы оплаты

--- app/ext/courses/views/__init__.py
+++ app/ext/courses/views/__init__.py
@@ -78,20 +78,17 @@ def course_details(course_id):
 
 @courses.route("/form-payment", methods=["GET", "POST"])
 def form_payment():
-     # Получаем все курсы и формируем их выбор
+    # Получаем все курсы и формируем их выбор
     courses = Course.query.all()
     course_choices = [(course.id, course.title) for course in courses]
 
-    # Генерируем словарь тарифов для курсов
-    # tariffs_by_course = {
-    #     course.id: [(tariff.id, f"{tariff.title} – {tariff.price}") for tariff in course.tariffes] # tariffes — не опечатка???
-    #     for course in courses
-    # }
-    tariffs_by_course = []
-    for course in courses:
-        for tariff in course.tariffes:
-            tariffs_by_course.append((tariff.id, f"{tariff.title} – {tariff.price}"))
-    current_app.logger.error(tariffs_by_course)
+    # Генерируем список тарифов для всех курсов
+    tariffs_by_course = [
+        (tariff.id, f"{tariff.title} – {tariff.price}")
+        for course in courses for tariff in course.tariffes
+    ]
+
+    current_app.logger.debug(tariffs_by_course)
 
     # Создаём форму
     form = CoursePaymentForm()
@@ -102,71 +99,73 @@ def form_payment():
         form.name.data = current_user.first_name
         form.email.data = current_user.email
 
-    if form.validate_on_submit():
-        # Проверка Honeypot поля
-        if form.hidden_field.data:
-            current_app.logger.warning("Honeypot triggered! Possible bot detected.")
-            flash("Ошибка при отправке формы. Попробуйте снова.", "danger")
-            return redirect(url_for("course.form_payment"))
+    if not form.validate_on_submit():
+        return render_template("public/payment.j2", form=form, tariffs_by_course=tariffs_by_course)
 
-        # Проверка временной метки
-        form_time = int(form.form_time.data)  # Получаем значение времени в миллисекундах
-        current_time = int(time.time() * 1000)  # Текущее время в миллисекундах
-        time_difference = current_time - form_time
+    # Проверка Honeypot поля
+    if form.hidden_field.data:
+        current_app.logger.warning("Honeypot triggered! Possible bot detected.")
+        flash("Ошибка при отправке формы. Попробуйте снова.", "danger")
+        return redirect(url_for("course.form_payment"))
 
-        if time_difference < 2000:  # Если разница меньше 2 секунд
-            current_app.logger.warning("Форма отправлена слишком быстро! Возможный бот.")
-            flash("Ошибка при отправке формы. Попробуйте снова.", "error")
-            return redirect(url_for("course.form_payment"))
+    # Проверка временной метки
+    form_time = int(form.form_time.data)  # Получаем значение времени в миллисекундах
+    current_time = int(time.time() * 1000)  # Текущее время в миллисекундах
+    time_difference = current_time - form_time
 
+    if time_difference < 2000:  # Если разница меньше 2 секунд
+        current_app.logger.warning("Форма отправлена слишком быстро! Возможный бот.")
+        flash("Ошибка при отправке формы. Попробуйте снова.", "error")
+        return redirect(url_for("course.form_payment"))
 
-        # Обработка отправки формы
-        selected_course_id = form.course_title.data
-        selected_price = form.price.data
+    # Проверка существования выбранного курса и тарифа
+    selected_course = Course.query.get(form.course_title.data)
+    if not selected_course:
+        flash("Выбранный курс не найден. Пожалуйста, выберите корректный курс.", "danger")
+        return redirect(url_for("course.form_payment"))
 
-        selected_course = Course.query.get_or_404(selected_course_id)
-        selected_tariff = None
-        if selected_course:
-            selected_tariff = next((tariff for tariff in selected_course.tariffes if tariff.id == selected_price), None)
-
-        if current_user.is_authenticated:
-            payment_db = Payment(
-                user_id=current_user.id,
-                course_id=selected_course.id,
-                tariff_id=selected_tariff.id,
-                status_payment=0
-            )
-            db.session.add(payment_db)
-            db.session.commit()
-        else:
-            flash("Для регистрации на курс — зайдите в личный кабинет.", "error")
-
-        # Формируем сообщение для отправки
-        send_message = (
-            f"Новая заявка на курс:\n"
-            f"Имя: {form.name.data}\n"
-            f"Email: {form.email.data}\n"
-            f"Курс: {selected_course.title if selected_course else 'Неизвестный курс'}\n"
-            f"Тариф: {selected_tariff.title if selected_tariff else 'Неизвестный тариф'}\n"
-            f"Цена: {selected_tariff.price if selected_tariff else 'Не указано'}\n"
-            f"Способ оплаты: {form.payment_method.data}"
-        )
-
-        # Отправка сообщения (в Telegram и Email)
-        send_to_telegram(send_message, send_to_admin=True)
-        send_to_email(
-            subject="Новая заявка на курс",
-            body=send_message,
-            recipients=['bogatyrevata@gmail.com'],
-            sender='klasstruedaru@gmail.com',
-            reply_to=['klasstruedaru@gmail.com']
-        )
-
-        flash("Заявка на оплату курса зарегистрирована", "success")
-        return redirect(url_for("core.thank_you"))
-
-    return render_template(
-        "public/payment.j2",
-        form=form,
-        tariffs_by_course=tariffs_by_course,
+    selected_tariff = next(
+        (tariff for tariff in selected_course.tariffes if tariff.id == form.price.data),
+        None
     )
+    if not selected_tariff:
+        flash("Выбранный тариф не относится к выбранному курсу. Пожалуйста, выберите корректный тариф.", "danger")
+        return redirect(url_for("course.form_payment"))
+
+    # Запись данных в базу
+    if current_user.is_authenticated:
+        payment_db = Payment(
+            user_id=current_user.id,
+            course_id=selected_course.id,
+            tariff_id=selected_tariff.id,
+            status_payment=0
+        )
+        db.session.add(payment_db)
+        db.session.commit()
+    else:
+        flash("Для регистрации на курс — зайдите в личный кабинет.", "error")
+        return redirect(url_for("course.form_payment"))
+
+    # Формирование сообщения
+    send_message = (
+        f"Новая заявка на курс:\n"
+        f"Имя: {form.name.data}\n"
+        f"Email: {form.email.data}\n"
+        f"Курс: {selected_course.title}\n"
+        f"Тариф: {selected_tariff.title}\n"
+        f"Цена: {selected_tariff.price}\n"
+        f"Способ оплаты: {form.payment_method.data}"
+    )
+
+    # Отправка сообщения (в Telegram и Email)
+    send_to_telegram(send_message, send_to_admin=True)
+    send_to_email(
+        subject="Новая заявка на курс",
+        body=send_message,
+        recipients=['bogatyrevata@gmail.com'],
+        sender='klasstruedaru@gmail.com',
+        reply_to=['klasstruedaru@gmail.com']
+    )
+
+    flash("Заявка на оплату курса зарегистрирована", "success")
+    return redirect(url_for("core.thank_you"))
